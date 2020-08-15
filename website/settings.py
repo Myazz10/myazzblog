4fbb1701f8655f59e8fc79102780a31cadd73e4f
Moi Myazz <moimyazz@gmail.com>
2020-08-15 14:53:28 -0500

    Tweaking

--- website/settings.py
+++ website/settings.py
@@ -14,12 +14,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+#DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://myazzblog.herokuapp.com/']
 
 
 # Application definition
